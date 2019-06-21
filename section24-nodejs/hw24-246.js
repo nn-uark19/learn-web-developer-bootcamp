503c36cbd55e3e89e52eda83654375f6dfe57baf
Nghia Nguyen <nhuunghia_1711@yahoo.com.vn>
2019-06-21 16:13:06 -0500

    node js

---
+++ section24-nodejs/hw24-246.js
@@ -0,0 +1,11 @@
+console.log('Begin nodejs');
+
+function print(word, times) {
+	for(var i=0; i<times; i++) {
+		console.log(word);
+	}
+};
+
+print("Echo!!!", 10)
+print("Tater Tots!!!", 3)
+console.log('End nodejs');
